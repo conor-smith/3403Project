544d78514f39050dbe5ca7271a804f1705069d2f
Conor <conormartindale@gmail.com>
2019-04-12 12:18:26 +0800

    A mistake was made

--- pair-up.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from app.models import Student, Project, Lab
-
-@app.shell_context_processor
-def make_shell_context():
-  return {'db':db, 'Student':Student, "Project":Project, 'Lab':Lab}
