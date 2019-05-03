2bdabcfa0de0f813668e976150f7f4e17d7e5d0a
Conor <conormartindale@gmail.com>
2019-05-03 16:39:26 +0800

    Added Login popup functionality using JQuery

--- global.js
+++ global.js
@@ -17,3 +17,23 @@ function currentTime()
   var n = d.toLocaleTimeString();
   document.getElementById("currenttime").innerHTML = "The current time is: " + n;
 }
+
+//Handles login popup
+$("document").ready(function()
+{
+  //Displays login popup and dims rest of page
+  $("#userlogin").click(function()
+  {
+    document.getElementById("cover").style.zIndex = "50"
+    document.getElementById("cover").style.opacity = "0.5"
+    document.getElementById("ulogin").style.display = "block"
+  })
+
+  //hides login popup
+  $("#cover").click(function()
+  {
+    document.getElementById("cover").style.zIndex = "-1"
+    document.getElementById("cover").style.opacity = "0"
+    document.getElementById("ulogin").style.display = "none"
+  })
+})
