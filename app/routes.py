1974ed19cf9e3770dd0c5c7b1f7c530cca93768a
Conor <conormartindale@gmail.com>
2019-05-13 12:16:27 +0800

    Added forms.py Changes to base.html and routes.py
    Added first form which seems to be funcitoning fine. Removed example user

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,16 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/front')
 def front():
-    example_user = {
-        "username" : "Billtone",
-        "isAdmin" : True
-    }
+    form = LoginForm()
     example_poll = {
         "img" : "img/poster.png"
     }
     polls = [example_poll, example_poll, example_poll, example_poll, example_poll, example_poll]
-    return render_template("front.html", title="Front Page", user=example_user, polls=polls)
+    return render_template("front.html", title="Front Page", polls=polls, form=form)
 
 @app.route('/register')
 def register():
