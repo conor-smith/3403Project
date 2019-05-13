bdbb8139cb6a2987004dcc1deba810c7f46778c6
Conor <conormartindale@gmail.com>
2019-05-13 23:06:32 +0800

    All of the tables seem to be at least functional (in the sense that they're no longer throwing out error messages). Whether the association tables work or not is yet to be seen. The position of the tables had to be shuffled around in order for the system to work properly

--- app/models.py
+++ app/models.py
@@ -5,34 +5,24 @@ from datetime import datetime
 from flask_login import UserMixin
 from app import login
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
-    password_hash = db.Column(db.String(128))
-    admin = db.Column(db.String(1), default = "F")
-    created_polls = db.relationship("Poll", backref = "author", lazy = "dynamic")
-    #participated_polls = db.relationship(
-    #    'Poll', secondary = UserPolls,
-    #    primaryjoin = (UserPolls.c.u_id == id),
-    #    secondaryjoin=(UserPolls.c.p_id == Poll.id),
-    #    backref = db.backref('participants', lazy='dynamic'), lazy='dynamic')
+userpolls = db.Table("userpolls",
+    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
+    db.Column("u_id", db.Integer, db.ForeignKey("user.id")),
+    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
+    db.Column("score", db.Integer)
+)
 
-    def __repr__(self):
-        return "<User {}>".format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+globalpolls = db.Table("globalpolls",
+    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
+    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
+    db.Column("score", db.Integer)
+)
 
 class Media(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(64), index = True)
     poster = db.Column(db.String(100), unique = True)
     mtype = db.Column(db.String(16), index = True)
-    genre = db.Column(db.String(32), index = True)
 
     def __repr__(self):
         return "<Media {}>".format(self.title)
@@ -42,25 +32,38 @@ class Poll(db.Model):
     name = db.Column(db.String(128))
     creator = db.Column(db.Integer, db.ForeignKey("user.id"))
     timestamp = db.Column(db.DateTime, default = datetime.utcnow)
-    genre = db.Column(db.String(32), index = True, default = "No Genre")
     mtype = db.Column(db.String(16), index = True, default = "All")
-    #choices = db.relationship("GlobalPolls", backref = "poll", lazy = "dynamic")
+    choices = db.relationship(
+        "Media", secondary = globalpolls,
+        primaryjoin = (globalpolls.c.p_id == id),
+        secondaryjoin = (globalpolls.c.m_id == Media.id),
+        backref = db.backref("polls", lazy = "dynamic"), lazy = "dynamic"
+    )
     
     def __repr__(self):
         return "<Poll {}>".format(self.name)
 
-#GlobalPolls = db.Table("globalpolls",
-#    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
-#    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
-#    db.Column("score", db.Integer)
-#)
-#
-#Userpolls = db.Table("userpolls",
-#    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
-#    db.Column("u_id", db.Integer, db.ForeignKey("user.id")),
-#    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
-#    db.Column("score", db.Integer)
-#)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64), index = True, unique = True)
+    password_hash = db.Column(db.String(128))
+    admin = db.Column(db.String(1), default = "F")
+    created_polls = db.relationship("Poll", backref = "author", lazy = "dynamic")
+    participated = db.relationship(
+        "Poll", secondary = userpolls,
+        primaryjoin = (userpolls.c.u_id == id),
+        secondaryjoin = (userpolls.c.p_id == Poll.id),
+        backref = db.backref("participants", lazy = "dynamic"), lazy = "dynamic"
+    )
+
+    def __repr__(self):
+        return "<User {}>".format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
