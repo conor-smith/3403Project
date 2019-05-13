9ac129d6fa40b3e03c324e916341383b8013f9f9
Conor <conormartindale@gmail.com>
2019-05-13 20:29:28 +0800

    Commentd out other tables as they were causing problems. Users are now able to log in

--- app/models.py
+++ app/models.py
@@ -2,15 +2,17 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 from datetime import datetime
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     password_hash = db.Column(db.String(128))
     admin = db.Column(db.String(1), default = "F")
-    created_polls = db.relationship("Poll", backref = "author", lazy = "dynamic")
-    participated_polls = db.relationship("UserPoll", backref = "author", lazy = "dynamic")
+    #created_polls = db.relationship("Poll", backref = "author", lazy = "dynamic")
+    #participated_polls = db.relationship("UserPoll", backref = "author", lazy = "dynamic")
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -21,37 +23,41 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-class Media(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(64), index = True)
-    poster = db.Column(db.String(100), unique = True)
-    mtype = db.Column(db.String(16), index = True)
-    genre = db.Column(db.String(32), index = True)
+#class Media(db.Model):
+#    id = db.Column(db.Integer, primary_key = True)
+#    title = db.Column(db.String(64), index = True)
+#    poster = db.Column(db.String(100), unique = True)
+#    mtype = db.Column(db.String(16), index = True)
+#    genre = db.Column(db.String(32), index = True)
+#
+#    def __repr__(self):
+#        return "<Media {}>".format(self.title)
+#
+#class Poll(db.Model):
+#    id = db.Column(db.Integer, primary_key = True)
+#    name = db.Column(db.String(128))
+#    creator = db.Column(db.Integer, db.ForeignKey("user.id"))
+#    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
+#    genre = db.Column(db.String(32), index = True, default = "No Genre")
+#    mtype = db.Column(db.String(16), index = True, default = "All")
+#    choices = db.relationship("GlobalPolls", backref = "poll", lazy = "dynamic")
+#    
+#    def __repr__(self):
+#        return "<Poll {}>".format(self.name)
+#
+#GlobalPolls = db.Table("globalpolls",
+#    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
+#    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
+#    db.Column("score", db.Integer)
+#)
+#
+#Userpolls = db.Table("userpolls",
+#    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
+#    db.Column("u_id", db.Integer, db.ForeignKey("user.id")),
+#    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
+#    db.Column("score", db.Integer)
+#)
 
-    def __repr__(self):
-        return "<Media {}>".format(self.title)
-
-class Poll(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128))
-    creator = db.Column(db.Integer, db.ForeignKey("user.id"))
-    timestamp = db.Column(db.DateTime, default = datetime.utcnow)
-    genre = db.Column(db.String(32), index = True, default = "No Genre")
-    mtype = db.Column(db.String(16), index = True, default = "All")
-    choices = db.relationship("GlobalPolls", backref = "poll", lazy = "dynamic")
-    
-    def __repr__(self):
-        return "<Poll {}>".format(self.name)
-
-GlobalPolls = db.Table("globalpolls",
-    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
-    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
-    db.Column("score", db.Integer)
-)
-
-Userpolls = db.Table("userpolls",
-    db.Column("p_id", db.Integer, db.ForeignKey("poll.id")),
-    db.Column("u_id", db.Integer, db.ForeignKey("user.id")),
-    db.Column("m_id", db.Integer, db.ForeignKey("media.id")),
-    db.Column("score", db.Integer)
-)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,31 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route('/')
 @app.route('/front')
 def front():
+    user = current_user
     example_poll = {
         "img" : "img/poster.png"
     }
     polls = [example_poll, example_poll, example_poll, example_poll, example_poll, example_poll]
-    return render_template("front.html", title="Front Page", polls=polls)
+    return render_template("front.html", user=user, title="Front Page", polls=polls)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("front"))
     lform = LoginForm()
+    if lform.validate_on_submit():
+        user = User.query.filter_by(username = lform.username.data).first()
+        if user is None or not user.check_password(lform.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("front"))
+        login_user(user, remember = lform.remember_me.data)
+        return redirect(url_for("front"))
     return render_template("login.html", title="Log in", lform=lform)
 
 @app.route('/register')
@@ -23,3 +35,8 @@ def register():
 @app.route('/about_us')
 def about_us():
     return render_template("about_us.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("front"))
