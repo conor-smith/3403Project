00b3c55c135d0019c11b4db1c463ec0902f51f0c
Conor <conormartindale@gmail.com>
2019-05-20 15:16:33 +0800

    Added requirements.txt Added small additional validation to forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 # https://github.com/wtforms/wtforms/blob/master/src/wtforms/fields/html5.py , it's undocumented on purpose apparently :shrug:
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, ValidationError, Length
 from app.models import User, Poll
 
 
@@ -37,7 +37,7 @@ class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[InputRequired()], 
         render_kw={"placeholder": "Username"})
     #email = EmailField("Email", validators=[InputRequired(), Email()], render_kw={"placeholder": "Email Address"})
-    password = PasswordField("Password", validators=[InputRequired()], 
+    password = PasswordField("Password", validators=[InputRequired(), Length(min = 8)], 
         render_kw={"placeholder": "Password"})
     password2 = PasswordField("Repeat Password", validators=[InputRequired(), EqualTo('password')], 
         render_kw={"placeholder": "Confirm Password"})
